85833dcbf3cf1ef6ae09e3fc56415f2195ec87f8
Sarialee <slee907@my.bcit.ca>
2019-09-22 16:10:58 -0700

    Redo of initial commit

---
+++ test.js
@@ -0,0 +1,22 @@
+// comp1320 quiz-1, Question #5 ---> trying again
+
+
+let userInput = 70;
+let TCook = 58;
+let BGates = 63;
+let EMusk = 48;
+
+if (userInput > BGates) {
+    console.log("You are older than Bill!");
+} else if (userInput > TCook) {
+    console.log("You are older than Tim, and maybe even Bill!");
+} else if (userInput % EMusk == 0) {
+    console.log("Your age is divisible by Elon's!");
+} else if (userInput % EMusk > 0) {
+    console.log("exiting the program now");
+}
+
+
+// Don't know how to use function yet.
+// Need to learn how to set varied data type.
+// (how to make object|string return "Please ensure your input is a Number data type.")
